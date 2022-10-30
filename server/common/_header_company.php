5aae547940d6f62133543abb72e6edc6e1bb0f67
ryuta-mats <matsu.r.1987@gmail.com>
2022-10-30 18:47:25 +0900

    カンパニーログイン機能実装

--- server/common/_header_company.php
+++ server/common/_header_company.php
@@ -9,14 +9,22 @@
             <ul class="menu-nav">
                 <li><a class="nav_link" href="../companys/company_appry_list.php">求人一覧</a></li>
                 <li><a class="nav_link" href="../companys/company_jobofer_list.php">応募者一覧</a></li>
-                <li><a class="nav_link" href="../companys/company_signup.php">新規登録</a></li>
-                <li><a class="nav_link" href="../companys/company_login.php">ログイン</a></li>
+                <?php if (!empty($login_company)) : ?>
+                    <li><a class="nav_link" href="../companys/company_edit.php?id=<?= $login_company['id'] ?>">登録情報変更</a></li>
+                    <li><a class="nav_link" href="../companys/company_logout.php">ログアウト</a></li>
+                <?php else : ?>
+                    <li><a class="nav_link" href="../companys/company_signup.php">新規登録</a></li>
+                    <li><a class="nav_link" href="../companys/company_login.php">ログイン</a></li>
+                <?php endif; ?>
+
                 <?php if ($login_company) : ?>
                     <li class="login_flag status_login">
-                        <img class="login_image" src="../images/niseko-ta.jpg" alt="<?= $login_company ?>">
-                        <div class="login_wrap">
-                            <p><?= $login_company ?></p>
-                        </div>
+                        <a class="status_user_login" href="../companys/company_show.php">
+                            <img class="login_image" src="../images/company/<?= h($login_company['image']) ?>" alt="<?= $login_company['name'] ?>">
+                            <div class="login_wrap">
+                                <p><?= h($login_company['name']) ?></p>
+                            </div>
+                        </a>
                     </li>
                 <?php else : ?>
                     <li class="login_flag status_logout">
--- server/companys/company_login.php
+++ server/companys/company_login.php
@@ -1,6 +1,39 @@
 <?php
 include_once __DIR__ . '/../common/functions.php';
-$login_company ='株式会社ニセコリゾート観光協会';
+// セッション開始
+session_start();
+
+$login_company = '';
+
+if (isset($_SESSION['current_company'])) {
+    $login_company = $_SESSION['current_company'];
+}
+// ログイン判定
+if (isset($_SESSION['current_company'])) {
+    header('Location: ../companys/company_jobofer_list.php');
+    exit;
+}
+
+$email = '';
+$password = '';
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+
+    $errors = company_login_validate($email, $password);
+
+    if (empty($errors)) {
+        $company = find_company_by_email($email);
+        if (!empty($company) && password_verify($password, $company['password'])) {
+            company_login($company);
+        } else {
+            $errors[] = MSG_EMAIL_PASSWORD_NOT_MATCH;
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -11,15 +44,31 @@ $login_company ='株式会社ニセコリゾート観光協会';
     <?php include_once __DIR__ . "/../common/_header_company.php" ?>
 
     <div id="main" class="wrapper">
-        <h2 class="title company_bg_title">ログイン</h2>
+        <div class="tit_wrap">
+            <h1 class="title company_bg_title">ログイン</h1>
+        </div>
+        <?php if (!empty($errors)) : ?>
+            <div class="login_err_wrap">
+                <ul class="err_msg">
+                    <?php foreach ($errors as $error) : ?>
+                        <li><i class="fa-solid fa-circle-exclamation"></i><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+
         <form class="form" method="post" action="">
             <label for="company_login_email">
                 <div class="form_title company_title">メールアドレス</div><span class="required">必須</span>
-                <input class="input_item" id="company_login_email" type="email" name="company_login_email">
+                <div class="input_item_wrap">
+                    <input class="input_item" id="company_login_email" type="email" name="email" value="<?= h($email); ?>">
+                </div>
             </label>
             <label for="company_login_password">
                 <div class="form_title company_title">パスワード</div><span class="required">必須</span>
-                <input class="input_item" id="company_login_password" type="password" name="company_login_password">
+                <div class="input_item_wrap">
+                    <input class="input_item" id="company_login_password" type="password" name="password">
+                </div>
             </label>
             <div id="login_send_wrap">
                 <input id="company_login_btn" class="bg_btn company_btn" type="submit" value="ログイン">
