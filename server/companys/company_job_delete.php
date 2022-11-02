c29d49662cd562127dfbc495e6afec7e74c16f9d
ryuta-mats <matsu.r.1987@gmail.com>
2022-11-02 14:53:48 +0900

    修正

--- server/companys/company_job_delete.php
+++ server/companys/company_job_delete.php
@@ -1,5 +1,7 @@
 <?php
 include_once __DIR__ . '/../common/functions.php';
+include_once __DIR__ . '/../common/functions_db.php';
+include_once __DIR__ . '/../common/functions_varidate.php';
 // セッション開始
 session_start();
 
