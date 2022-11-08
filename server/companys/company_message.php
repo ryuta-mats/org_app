5369cd7587ad69227e29c66afd7779899ea2606e
ryuta-mats <matsu.r.1987@gmail.com>
2022-11-08 14:40:40 +0900

    カンパニーメッセージ送信機能実装

--- server/companys/company_message.php
+++ server/companys/company_message.php
@@ -1,31 +1,36 @@
 <?php
 include_once __DIR__ . '/../common/functions.php';
-$user_name = '松本竜太';
-$login_company ='株式会社ニセコリゾート観光協会';
+// セッション開始
+session_start();
 
-$company_name = 'ニセコ株式会社';
+if (empty($_SESSION['current_company'])) {
+    header('Location: ../companys/company_login.php');
+    exit;
+} elseif (empty($_GET['appry_id'])) {
+    header('Location: ../companys/company_appry_list.php');
+    exit;
+}
+$appry_id = $_GET['appry_id'];
+$login_company = $_SESSION['current_company'];
+$id = $_SESSION['current_company']['id'];
 
-$messages = array(
-    0 => array(
-        'body' => 'この度は弊社求人へのご応募誠にありがとうございます。是非面接させていただきたいのですが、平日はお時間はありますか？オンラインでも可能です。ニセコ株式会社人事部松本',
-        'datetime' => '2022.9.21 13:00',
-        'class' => 0,
-        'name' => $login_company,
-    ),
-    1 => array(
-        'body' => 'こんにちは。連絡してくれてめっちゃ嬉しいです。まじ感謝です。平日は仕事なんで無理っす。土日でお願いできますか？よろしくっす。',
-        'datetime' => '2022.9.22 23:00',
-        'class' => 1,
-        'name' => $user_name,
-    ),
-    2 => array(
-        'body' => 'ご返信ありがとうございます。ご都合がつかないようですので、今回は結構です。ありがとうございました。ニセコ株式会社人事部松本',
-        'datetime' => '2022.9.23 9:00',
-        'class' => 0,
-        'name' => $login_company,
-    ),
-);
+$appry = find_appry_by_appry_id($appry_id);
+$messages = find_message_by_appry_id($appry_id);
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $body = filter_input(INPUT_POST, 'body');
+    $errors = message_validate($body);
+
+    if (empty($errors)) {
+        $msg_from = 1; //送信元(0=USER、1=COMPANY)
+        //関数でデータベースにinsert
+        if (inssert_message($body, $appry, $msg_from)) {
+            //自分をリダイレクト
+            header('Location: company_message.php?appry_id=' . $appry['appry_id']);
+            exit;
+        }
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -35,22 +40,50 @@ $messages = array(
 <body>
 
     <?php include_once __DIR__ . "/../common/_header_company.php" ?>
-
     <div id="main" class="wrapper">
         <div class="tit_wrap">
-            <h1 class="title company_bg_title"><span>message</span><?= h($user_name) ?>とのメッセージ</h1>
+            <h1 class="title company_bg_title"><span>message</span><?= h($appry['company']) ?>さんと<?= h($appry['user']) ?>さんとのメッセージ</h1>
         </div>
         <div class="msg_wrap">
+            <?php if (empty($messages)) : ?>
+                <div>メッセージなし</div>
+            <?php endif; ?>
+            <?php if (!empty($errors)) : ?>
+                <div class="login_err_wrap">
+                    <ul class="err_msg">
+                        <?php foreach ($errors as $error) : ?>
+                            <?php foreach ($error as $val) : ?>
+                                <li><i class="fa-solid fa-circle-exclamation"></i><?= $val ?></li>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
             <?php foreach ($messages as $message) : ?>
-                <div class="message <?php echo $message['class'] == 1 ? 'opposite_message' : 'my_message'; ?>">
-                    <div class="message_body <?php echo $message['class'] == 1 ? 'opposite_message_body' : 'my_message_body'; ?>">
+                <div class="message <?php echo $message['msg_from'] == 0 ? 'opposite_message' : 'my_message'; ?>">
+                    <div class="message_body <?php echo $message['msg_from'] == 0 ? 'opposite_message_body' : 'my_message_body'; ?>">
                         <p><?= h($message['body']) ?></p>
                     </div>
-                    <p class="message_datetime"><?= $message['datetime'] ?> <?= $message['name'] ?></p>
+                    <p class="message_datetime">
+                        <?= $message['created_at'] ?>
+                        <?php if ($message['msg_from'] == 0) : ?>
+                            <?= $message['user'] ?>
+                        <?php else : ?>
+                            <?= $message['company'] ?>
+                        <?php endif; ?>
+                    </p>
                 </div>
             <?php endforeach; ?>
-            <form class="message my_message" action="" method="POST">
-                <textarea name="send_message_body" id="send_message_body" cols="50" rows="10" placeholder="メッセージを入力して下さい。"></textarea>
+            <form class="message my_message" action="company_message.php?appry_id=<?= $appry_id ?>" method="POST">
+                <textarea class="<?php empty($errors['body']) ?: print 'err_input'; ?>" name="body" id="send_message_body" cols="50" rows="10" placeholder="メッセージを入力して下さい。"></textarea>
+                <?php if (!empty($errors['body'])) : ?>
+                    <ul class="err_msg">
+                        <?php foreach ($errors['body'] as $error) : ?>
+                            <li><i class="fa-solid fa-circle-exclamation"></i><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
                 <input class="bg_btn company_btn message_send_btn" type="submit">
             </form>
         </div>
