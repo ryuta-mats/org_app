413a1d4573d4b9271479a640edb1e7a3d642db16
ryuta-mats <matsu.r.1987@gmail.com>
2022-11-20 01:53:50 +0900

    求人詳細ページレスポンシブ対応

--- server/users/_appry_detail.php
+++ server/users/_appry_detail.php
@@ -1,67 +1,56 @@
-<table class="table ">
-    <tbody>
-        <tr class="element">
-            <th>会社名</th>
-            <td>
-                <div class="job_info_item">
-                    <p><?= h($company['name']) ?></p>
+<dl class="job_detail_body">
+    <?php if (!empty($job['image'])) : ?>
+        <div class="job_detail_wrap">
+            <dd class="job_detail_content">
+                <div class="appry_image">
+                    <img src="../images/job/<?= h($job['image']) ?>" alt="<?= h($company['name']) ?>">
                 </div>
-            </td>
-        </tr>
-        <tr class="element">
-            <th>募集職種</th>
-            <td>
-                <div class="job_info_item">
-                    <p><?= h($job['name']) ?></p>
-                </div>
-            </td>
-        </tr>
-        <tr class="element">
-            <th>給与</th>
-            <td>
-                <div class="job_info_item">
-                    <p><?= h($category['name']) ?><?= h($job['price']) ?>円</p>
-                </div>
-            </td>
-        </tr>
-        <tr class="element">
-            <th>仕事内容</th>
-            <td>
-                <div class="job_info_item">
-                    <p><?= h($job['profile']) ?></p>
-                </div>
-            </td>
-        </tr>
-        <tr class="element">
-            <th>募集期間</th>
-            <td>
-                <div class="job_info_item">
-                    <p><?= h($job['start_date']) ?>～ <?= h($job['end_date']) ?></p>
-                </div>
-            </td>
-        </tr>
-        <tr class="element">
-            <th>企業ホームページ</th>
-            <td>
-                <div class="job_info_item">
-                    <?php if (empty($company['url'])) : ?>
-                        登録無し
-                    <?php else : ?>
-                        <a href="<?= h($company['url']); ?>"><?= h($company['url']); ?></a>
-                    <?php endif; ?>
-                </div>
-            </td>
-        </tr>
-        <?php if (!empty($job['image'])) : ?>
-            <tr class="element">
-                <th>画像</th>
-                <td>
-                    <div class="appry_image">
-                        <img src="../images/job/<?= h($job['image']) ?>" alt="<?= h($company['name']) ?>">
-                    </div>
-                </td>
-            </tr>
-        <?php endif; ?>
-
-    </tbody>
-</table>
+            </dd>
+        </div>
+    <?php endif; ?>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">会社名</dt>
+        <dd class="job_detail_content"><?= h($company['name']) ?></dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">募集職種名</dt>
+        <dd class="job_detail_content"><?= h($job['name']) ?></dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">給料</dt>
+        <dd class="job_detail_content"><?= h($category['name']) ?><?= h($job['price']) ?>円</dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">仕事内容</dt>
+        <dd class="job_detail_content"><?= h($job['profile']) ?></dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">勤務地</dt>
+        <dd class="job_detail_content">
+            <p><?= h($job['area']) ?></p>
+        </dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">人事担当者</dt>
+        <dd class="job_detail_content">
+            <p><?= h($company['manager_name']) ?></p>
+        </dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">募集期間</dt>
+        <dd class="job_detail_content">
+            <p><?= h($job['start_date']) ?> から</p>
+            <p><?= h($job['end_date']) ?> まで</p>
+        </dd>
+    </div>
+    <div class="job_detail_wrap">
+        <dt class="job_detail_title">企業ホームページ</dt>
+        <dd class="job_detail_content">
+            <?php if (empty($company['url'])) : ?>
+                登録無し
+            <?php else : ?>
+                <a href="<?= h($company['url']); ?>"><?= h($company['url']); ?></a>
+            <?php endif; ?>
+        </dd>
+    </div>
+</dl>
